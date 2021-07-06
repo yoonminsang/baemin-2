05e17c52e8e07abc398dced7e12a8f056acfc842
yoonminsang <apdlyooapdl@naver.com>
2021-07-07 00:48:18 +0900

    Make login markdown

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,7 @@
 import express from 'express';
 const router = express.Router();
 router.get('/', (req, res) => {});
+router.get('/login', (req, res) => {
+  res.render('login', { title: '로그인 페이지' });
+});
 export default router;
