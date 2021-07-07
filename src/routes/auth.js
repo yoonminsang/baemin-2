681b8ab1568ea4b9cabf4ca293b67ee56047fcc8
yoonminsang <apdlyooapdl@naver.com>
2021-07-07 11:36:48 +0900

    Solve conflict

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,4 +4,7 @@ router.get('/', (req, res) => {});
 router.get('/login', (req, res) => {
   res.render('login', { title: '로그인 페이지' });
 });
+router.get('/signup/terms', (req, res) => {
+  res.render('signup-terms', { title: '회원가입 약관동의' });
+});
 export default router;
