a018d7c4bb3131a8be8f299f8d7d69e51b303cd6
junbeoman <wnsqja8744@gmail.com>
2021-07-07 11:18:49 +0900

    update signup verify router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,7 @@
 import express from 'express';
 const router = express.Router();
 router.get('/', (req, res) => {});
+router.get('/signup/verify', (req, res) => {
+    res.render('signup-verify', { title: '회원가입 페이지'});
+});
 export default router;
