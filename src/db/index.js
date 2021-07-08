acd4d29e8af57f9fe388377d14fd172226e27821
yoonminsang <apdlyooapdl@naver.com>
2021-07-08 18:16:22 +0900

    if not exist make db folder

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,7 @@
+import fs from 'fs';
+
+!fs.existsSync('db') && fs.mkdirSync('db');
+
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db/db.json');
